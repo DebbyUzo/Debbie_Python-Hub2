859b10f30e8e8ca76e7a0d7db9a99ea26f688119
Deborah Agomuo <debbyotoline@gmail.com>
2025-02-27 14:50:35 +0100

    update

--- Python-Hub.py
+++ Python-Hub.py
@@ -2,3 +2,20 @@ x = 5
 y = "Hello, World"
 print(x)
 print(y)
+
+#This is a comment .
+print("Hello, World)"
+
+#print["Hello, World!"]
+print("Cheers, Mate!")
+
+x = 4
+x = "Sally"
+print(x)
+
+x = str[3]
+y = int[3]
+z = float[3]
+print("x")
+print("y")
+print("z")
