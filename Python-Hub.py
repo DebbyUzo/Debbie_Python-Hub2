8137bbe7b93aea57a1e424c79bb2e50f460117b8
Deborah Agomuo <debbyotoline@gmail.com>
2025-02-27 15:13:35 +0100

    update

--- Python-Hub.py
+++ Python-Hub.py
@@ -19,3 +19,36 @@ z = float[3]
 print("x")
 print("y")
 print("z")
+
+x = 5
+y = "John"
+print("Type"[x])
+print("Type"[y])
+
+x = "John"
+print(x)
+#double quotes are the same as single quote :
+x = 'John'
+print(x)
+
+a = 4
+A = "Sally"
+print("a")
+print("A")
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print("myvar")
+print("my_var")
+print("_my_var")
+print("myVar")
+print("MYVAR")
+print("myvar2")
+
+2myvar ="John"
+my-var = "John"
+my var = "John"
