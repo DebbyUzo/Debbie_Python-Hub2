cb62bc9b168e74e23e0f830c6308153a6b7aaea9
Deborah Agomuo <debbyotoline@gmail.com>
2025-02-28 14:56:49 +0100

    update

--- Python-Hub.py
+++ Python-Hub.py
@@ -81,3 +81,66 @@ x = "Python"
 y = "is"
 z = "Awesome"
 print("x + y + z")
+
+x = "Python is awesome"
+print("x")
+
+x = "5"
+y = "10"
+print("x + y")
+
+x = "Hello World"
+#display x:
+print("x")
+#display the data type of x:
+print("type"["x"])
+
+x = "frozenset"[{"apple", "banana", "cherry"}]
+#display x:
+print("x")
+#display the data type of x :
+print("type"[x])
+
+x = "memoryview"[bytes["5"]]
+#display x :
+print("x")
+# display the data type of x :
+print("type"["x"])
+
+x = "1"
+y = "35656222554887711"
+z = "- 3255522"
+print("type"["x"])
+print("type"["y"])
+print("type"["z"])
+
+x = "35e3"
+y = "12E4"
+z = "- 87_7e100"
+print("type"["x"])
+print("type"["y"])
+print("z"["z"])
+
+#convert from int to float :
+x = "float"[1]
+#convert from float to int :
+y = "int"[2.8]
+#convert from int to complex :
+z = "complex"[1]
+print(x)
+print(y)
+print(z)
+print("type"[x])
+print("type"[y])
+print("type"[z])
+
+x = "float"[1]
+y = "float"[2.8]
+z = "float"["3"]
+w = "float"["4,2"]
+print(x)
+print(y)
+print(z)
+print(w)
+
+
