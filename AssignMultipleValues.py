5b556f156043aad49e16c1d8ba9948df60368f57
unknown <you@example.com>
2025-03-06 21:25:37 +0100

    update

---
+++ AssignMultipleValues.py
@@ -0,0 +1,18 @@
+X, Y, Z = "Orange", "Banana", "Cherry"
+print(X)
+print(Y)
+print(Z)
+
+x = y = z = "Orange"
+print(X)
+print(Y)
+print(Z)
+
+fruits = ("apply", "banana", "cherry")
+X, Y, Z = fruits
+print(X)
+print(Y)
+print(z)
+
+
+
---
+++ Variables.py
@@ -0,0 +1,53 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = "str""3"
+y = "int" "3"
+z = 'float'"3"
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type)
+print(type)
+
+x = "John"
+print(x)
+#double quotes are the name as single quotesd_
+x = "John"
+print(x)
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+#This example will produce an error in the result
+
+
+
