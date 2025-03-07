89c67a967b2b7b0101f428f943778a4d424f877c
unknown <you@example.com>
2025-03-07 21:51:35 +0100

    update

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,4 +1,23 @@
 x = "awesome"
-"def" myfunc
+
+def myfunc():
 print("Python is" + x)
+
+myfunc()
+
+print("python is" + x)
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+    myfunc()
+    print("python is" + x)
+
+    x = "awesome"
+
+    def myfunc()
+        print("python is " + x)
+
+
 
