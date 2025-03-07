40d2af178d5f4425e4d6de81a483e74659e08957
unknown <you@example.com>
2025-03-07 15:38:08 +0100

    update

--- Variables.py
+++ Variables.py
@@ -28,26 +28,9 @@ print(x)
 
 a = 4
 A = "Sally"
+
 print(a)
 print(A)
-
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
-print(myvar)
-print(my_var)
-print(_my_var)
-print(myVar)
-print(MYVAR)
-print(myvar2)
-
-"2"myvar = "John"
-my-var = "John"
-my var = "John"
-#This example will produce an error in the result
 
 
 
