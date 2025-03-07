6d8f938f64bc33a2f094626df180dfae386fdcca
unknown <you@example.com>
2025-03-07 15:13:01 +0100

    update

--- DateTypes.py
+++ DateTypes.py
@@ -84,4 +84,101 @@ print(x)
 #display the data type of x:
 print(type(x))
 
+x = int(20)
 
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = list(("apple", "banaba", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x =  tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = dict(name= "john", aga=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = set({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+
+x = bool(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = bytes(5)
+
+#display x:
+print(x)
+
+#display the data type x:
+print(type(x))
+
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data of x:
+print(type(x))
+
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
